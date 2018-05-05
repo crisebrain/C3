8cea29b566655d211e378c125dc04b1a6e762ee7
ramonapariciog <moncho_apa@hotmail.com>
2018-05-05 00:47:47 -0500

    limpieza de main_fase1

--- SearchEngine/CognitiveSearchEngine.py
+++ SearchEngine/CognitiveSearchEngine.py
@@ -5,18 +5,21 @@ Created on Sun Apr 22 15:33:33 2018
 @author: crisebrain
 """
 
-def cognitive_req(respuesta):
+def cognitive_req(data):
     """
-    recive un arreglo json que contiene al menos un campo
+    recibe un arreglo json que contiene al menos un campo
     y cuyo campo es 'msgOriginal'
     """
-    msg = data['msgOriginal']
-
-
-    nodo1 = {"name":"Ampliacion", "value": respuesta,
-             "parent":"TarjetaCredito",
-             "idChatBot":idChatBot,
-             "msgAns":"Tu respuesta es {0}".format(respuesta)}
-    print("El mensaje original recibido es:", nodo1["msgAns"])
-    #nodosList = [nodo1]
-    return nodo1
+    msg = data['msgReq']
+    print("El mensaje original recibido es:", msg)
+    value = 20000
+    nodo1 = {"idField": "credito",
+             "value": 20000,
+             "name": "Ampliacion",
+             "acc": 98.7,
+             "mandatory": "direccion",
+             "txtEntrada": msg,
+             "msgAns": "Si hay ampliacion por %d" %value
+             }
+    nodosList = [nodo1]
+    return nodosList
--- main_fase1.py
+++ main_fase1.py
@@ -2,44 +2,21 @@
 from Utils import SessionContainer
 from InfoManager import InfoManager
 from ChatBotWrapper import ChatBotWrapper
+from SearchEngine import cognitive_req
 # from ConversationEngine import ChatBotWrapper
 
-def cognitive_req(data):
-    """
-    recibe un arreglo json que contiene al menos un campo
-    y cuyo campo es 'msgOriginal'
-    """
-    msg = data['msgReq']
-    print("El mensaje original recibido es:", msg)
-    value = 20000
-    nodo1 = {"idField": "credito",
-             "value": 20000,
-             "name": "Ampliacion",
-             "acc": 98.7,
-             "mandatory": "direccion",
-             "txtEntrada": msg,
-             "msgAns": "Si hay ampliacion por %d" %value
-             }
-    nodosList = [nodo1]
-    return nodosList
-
 if __name__ == "__main__":
+    # Por ahora con estas dos funciones se puede trabajar
     chbw = ChatBotWrapper("chatbots")
     im = InfoManager(SessionContainer, chbw)
+
+    # este control lo va a llevar
+    # el usuario con las peticions
     i = 0
     qs = len((im.sc.extractTree()).orderlist)
-    ids = ["name", "action", "provider_name", "affiliation"]
     while(i < qs):
+    # -------------------------------------------
         json_data = chbw.interceptIntent(im)
         im.intentFlow(json_data, chbw, cognitive_req)
         i += 1
     im.sc.ShowSessionTree()
-
-
-# im = InfoManager(SessionContainer, ChatBotWrapper, cognitive_req)
-# jdata = im.newconsult()
-# im.newAnswer(jdata)
-# jdata = im.newconsult()
-# im.newAnswer(jdata)
-# jdata = im.newconsult()
-# im.newAnswer(jdata)
