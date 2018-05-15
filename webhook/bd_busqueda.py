eb81ab0bcdcea314986935b0ee40d1dfa3d91fd1
ramonapariciog <moncho_apa@hotmail.com>
2018-05-15 09:52:05 -0500

    funcionalidad basica faseUno. Falta un pequeño test

--- webhook/bd_busqueda.py
+++ webhook/bd_busqueda.py
@@ -1,11 +1,37 @@
 import pandas as pd
 import json
+import re
+
+def joinnames(array):
+    stname = " ".join(array)
+    return stname
+
 
 class BDbusquedas:
     def __init__(self):
         self.BD = pd.read_csv("basedatosficticia.txt", header=0)
 
     def busqueda(self, valor):
-        posibles = self.BD.loc[self.BD.Nombre == valor]
+        valor = valor.strip()
+        Nombres = self.BD[["Nombre", "Apellido"]].apply(func=joinnames, axis=1)
+        posibles = self.BD.iloc[[True if valor.lower() in n else False for n in Nombres.str.lower().values]]
+        posiblesc = self.BD.iloc[[True if n in valor.lower() else False for n in Nombres.str.lower().values]]
+        posibles = pd.concat([posibles, posiblesc])
         resultado = json.loads(posibles.drop(["Id"], axis=1).to_json(orient="table"))["data"]
         return resultado
+
+# print(valor)
+# if len(valor.split(" ")) == 1:
+#     if valor.lower() in self.BD.Nombre.str.lower().values:
+#         posibles = self.BD.loc[self.BD.Nombre.str.lower() == valor.lower()]
+#         resultado = json.loads(posibles.drop(["Id"], axis=1).to_json(orient="table"))["data"]
+#     elif valor.lower() in self.BD.Apellido.str.lower().values:
+#         posibles = self.BD.loc[self.BD.Apellido.str.lower() == valor.lower()]
+#         resultado = json.loads(posibles.drop(["Id"], axis=1).to_json(orient="table"))["data"]
+#     else:
+#         resultado = []
+# elif len(valor.split(" ")) == 2:
+#     cad = valor.split(" ")
+#     posibles = self.BD.loc[self.BD.Nombre.str.lower() == cad[0].lower()]
+#     posibles2 = posibles.loc[posibles.Apellido.str.lower() == cad[1].lower()]
+#     resultado = json.loads(posibles2.drop(["Id"], axis=1).to_json(orient="table"))["data"]
--- webhook/webhook_dialogflow.py
+++ webhook/webhook_dialogflow.py
@@ -1,40 +1,52 @@
 import urllib
 import json
 import os
-from flask import Flask
-from flask import request
-from flask import make_response
+from flask import Flask, request, make_response
 
 app = Flask(__name__)
 
 @app.route("/webhook", methods=["POST", "GET"])
 def webhook():
     req = request.get_json(silent=True, force=True)
-    # print("Request")
-    # print(json.dumps(req, indent=4))
-    res = makeWebhookResult(req)
-    res = json.dumps(res, indent=4)
-    r = make_response(res)
+    print(json.dumps(req, indent=4))
+    try:
+        res = makeWebhookResult(req)
+        res = json.dumps(res, indent=4)
+        r = make_response(res)
+    except Exception as err:
+        res = {"payload": {"result": "Null", "returnCode": "0"},
+               "fulfillmentText": "Null"}
+        res = json.dumps(res, indent=4)
+        r = make_response(res)
     r.headers["Content-Type"] = "application/json"
     return r
 
 def makeWebhookResult(req):
-    if req.get("queryResult").get("action") == "VDN":
-        return makeresponseVDN(req)
+    action = req.get("queryResult").get("action")
+    if action == "VDN" or action == "saldo":
+        return makeresponseAction(req, action)
+    else:
+        return {"payload": {"result": "Null", "returnCode": "0"},
+               "fulfillmentText": "Null"}
 
-def makeresponseVDN(req):
+def makeresponseAction(req, action):
     from bd_busqueda import BDbusquedas
     bd = BDbusquedas()
+    # Carga de base de datos
     result = req.get("queryResult").get("parameters")
-    coincidencias = bd.busqueda(result.get("personname"))
+    coincidencias = bd.busqueda(result.get("nombre"))
     print(json.dumps(coincidencias, indent=4))
     returnCode = calcCode(coincidencias)
-    textresp = mensajson(coincidencias, returnCode)
-    resp = {"result":coincidencias,
-            "returnCode": returnCode,
-            "source": req.get("queryResult").get("intent").get("displayName"),
-            "displayText": textresp,
-            "speech": textresp}
+    textresp = mensajson(coincidencias, returnCode, action)
+    resultarray = []
+    for coin in coincidencias:
+        resultarray.append(dict(zip(["nombre", action],
+                                    [coin["Nombre"], coin[action]])))
+
+    resp = {#"speech": textresp,
+            "payload": {"result": resultarray, "returnCode": returnCode},
+            "fulfillmentText": textresp}
+            #"source": "telegram"}#req.get("queryResult").get("intent").get("displayName")}
     return resp
 
 def calcCode(array):
@@ -49,20 +61,35 @@ def calcCode(array):
         code = 3
     return code
 
-def mensajson(array, code):
-    if code == 0:
-        Text = "No hay referencia de esa persona"
-    elif code == 1:
-        elem = array[0]
-        Text = "El telefono de {0} {1} es {2}".format(elem["Nombre"],
-                                                      elem["Apellido"],
-                                                      elem["telefono"])
-    elif code == 2:
-        textnames = [elem["Nombre"] + " " + elem["Apellido"] for elem in array]
-        textnames = ' o '.join(textnames)
-        Text = "¿A cual persona te refieres? a {0}".format(textnames)
-    else:
-        Text = "Tengo {0} coincidencias, necesitas ser mas especifico".format(len(array))
+def mensajson(array, code, action):
+    if action == "VDN":
+        if code == 0:
+            Text = "No hay referencia de esa persona"
+        elif code == 1:
+            elem = array[0]
+            Text = "El telefono de {0} {1} es {2}".format(elem["Nombre"],
+                                                          elem["Apellido"],
+                                                          elem["VDN"])
+        elif code == 2:
+            textnames = [elem["Nombre"] + " " + elem["Apellido"] for elem in array]
+            textnames = ' o '.join(textnames)
+            Text = "¿A cual persona te refieres? a {0}".format(textnames)
+        else:
+            Text = "Tengo {0} coincidencias, necesitas ser mas especifico".format(len(array))
+    elif action == "saldo":
+        if code == 0:
+            Text = "No hay referencia de esa persona"
+        elif code == 1:
+            elem = array[0]
+            Text = "El saldo de {0} {1} es {2} pesos".format(elem["Nombre"],
+                                                             elem["Apellido"],
+                                                             elem["saldo"])
+        elif code == 2:
+            textnames = [elem["Nombre"] + " " + elem["Apellido"] for elem in array]
+            textnames = ' o '.join(textnames)
+            Text = "¿A cual persona te refieres? a {0}".format(textnames)
+        else:
+            Text = "Tengo {0} coincidencias, necesitas ser mas especifico".format(len(array))
     return Text
 
 if __name__ == "__main__":
