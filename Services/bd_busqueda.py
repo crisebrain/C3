3e707ac1630be1d658a61b4967d71c0a8e184842
ramonapariciog <moncho_apa@hotmail.com>
2018-05-30 09:51:34 -0500

    correccion_lectura_servicio_de_prueba

--- Services/bd_busqueda.py
+++ Services/bd_busqueda.py
@@ -3,9 +3,17 @@ import json
 import re
 import os
 
-def joinnames(array):
-    stname = " ".join(array)
-    return stname
+def joinnames(nameString):
+    if not isinstance(nameString, str):
+        stname = " ".join(nameString)
+        array = [word.strip() for word in stname.split(" ")
+                 if re.match(pattern=r"\w+", string=word)]
+        stname = " ".join(array)
+    else:
+        array = [word.strip() for word in nameString.split(" ")
+                 if re.match(pattern=r"\w+", string=word)]
+        stname = " ".join(array)
+    return normaliza(stname)
 
 def normaliza(data):
     import unicodedata
@@ -16,19 +24,25 @@ class BDbusquedas:
     def __init__(self):
         pathfile = os.getcwd()
         # pathfile = os.path.join(pathfile, "Services", "basedatosficticia.txt")
-        pathfile = os.path.join(pathfile, "Services", "DataSetExtensiones_utf8.csv")
+        pathfile = os.path.join(pathfile,
+                                "Services",
+                                "DataSetExtensiones_utf8.csv")
         self.BD = pd.read_csv(pathfile, header=0)
         # self.BD = pd.read_csv(pathfile, header=0, encoding="ISO-8859-1")
 
     def busqueda(self, valor):
-        valor = valor.strip()
-        valor = normaliza(valor)
+        valor = joinnames(valor)
+        print(valor)
         Nombres = self.BD[["Nombre", "Apellido"]].apply(func=joinnames, axis=1)
-        posibles = self.BD.iloc[[True if valor.lower() in n else False for n in Nombres.str.lower().values]]
-        # posiblesc = self.BD.iloc[[True if n in valor.lower() else False for n in Nombres.str.lower().values]]
+        print(Nombres)
+        posibles = self.BD.iloc[[True if valor.lower() in n else False
+                                 for n in Nombres.str.lower().values]]
+        # posiblesc = self.BD.iloc[[True if n in valor.lower() else False
+                                #   for n in Nombres.str.lower().values]]
         # posibles = pd.concat([posibles, posiblesc])
-        resultado = json.loads(posibles.drop(["Id"], axis=1).to_json(orient="table"))["data"]
-        return resultado
+        resultado = json.loads(posibles.drop(["Id"],
+                               axis=1).to_json(orient="table"))
+        return resultado["data"]
 
 def dbquery(valor):
     bdbusqueda = BDbusquedas()
