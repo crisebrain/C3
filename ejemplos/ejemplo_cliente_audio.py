12e42e534e97f957ec8bf8474759f01e6a425534
Erick Victor Gabriel Osorio Merlos <evgom.sid@gmail.com>
2018-06-06 14:25:50 -0500

    Cliente de ejemplo que envía audio para ser procesador por DF.

---
+++ ejemplos/ejemplo_cliente_audio.py
@@ -0,0 +1,49 @@
+import requests
+import json
+import subprocess
+import base64
+
+
+def encode_audio64_DF(archivo):
+    audio = open(archivo, "rb")
+    audio_content = audio.read()
+    return str(base64.b64encode(audio_content), "ascii", "ignore") # Borra saltos de linea
+
+
+def obtenemosToken():
+    # Obtenemos token de google (Ver documentación de como generar el token)
+    token = subprocess.check_output(["gcloud", "auth", "print-access-token"])
+    token = token.decode("utf-8").strip()
+    return token
+
+
+def getIntent(valoresAudio, token):
+    # ---------------------------------------------------------------------
+    headers = {"Content-Type": "application/json",
+               "Authorization": "Bearer " + token,
+               "charset": "UTF-8"}
+    req = requests.post("https://dialogflow.googleapis.com/v2/projects/transferenciaautomatica2/agent/sessions/4fe0f059:detectIntent",
+                        data=json.dumps(valoresAudio, indent=4), headers=headers)
+    return req
+
+
+# Inicialización
+token = obtenemosToken()
+audioBase64 = encode_audio64_DF("saldo8.wav")
+
+valores = {
+            "queryInput":{
+                "audioConfig": {
+                    "audioEncoding": "AUDIO_ENCODING_LINEAR_16",
+                    "sampleRateHertz": 8000,
+                    "languageCode": "es"
+                }
+            },
+            "inputAudio": audioBase64
+        }
+
+
+# Petición
+res = getIntent(valores, token)
+resJson = json.dumps(res.json(), indent=4)
+print(resJson.encode('ascii').decode('unicode-escape')) # Imprime correctamente los caracteres
