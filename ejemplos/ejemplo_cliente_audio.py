819b8822be70d60ed9785185c64b6dd7e63c7ef6
Erick Victor Gabriel Osorio Merlos <evgom.sid@gmail.com>
2018-06-11 10:54:31 -0500

    Ejemplos de clientes creados manualmente y con SDK. La ventaja de hacerlo con SDK es que se quita la depedencia de gcloud. Arregla el workaround.

---
+++ ejemplos/ejemplo_cliente_audio.py
@@ -0,0 +1,53 @@
+import requests
+import json
+import subprocess
+import base64
+
+
+def encode_audio64_DF(archivo):
+    audio = open(archivo, "rb")
+    audio_content = audio.read()
+    return str(base64.b64encode(audio_content), "ascii", "ignore") # Borra saltos de linea
+
+
+def obtenemosToken():
+    # Obtenemos token de google (Ver documentación de como generar el token)
+    token = subprocess.check_output(["gcloud", "auth", "print-access-token"])
+    token = token.decode("utf-8").strip()
+    return token
+
+
+def getIntent(valoresAudio, token):
+    # ---------------------------------------------------------------------
+    headers = {"Content-Type": "application/json",
+               "Authorization": "Bearer " + token,
+               "charset": "UTF-8"}
+    req = requests.post("https://dialogflow.googleapis.com/v2/projects/transferenciaautomatica2/agent/sessions/4fe0f059:detectIntent",
+                        data=json.dumps(valoresAudio, indent=4), headers=headers)
+    return req
+
+
+# Inicialización
+token = obtenemosToken()
+audioBase64 = encode_audio64_DF("audios/saldo16.flac")
+
+# Actualizar los valores dependiendo el audio a procesar
+# https://dialogflow.com/docs/reference/api-v2/rest/v2/projects.agent.sessions/detectIntent
+# sample rate ideal en 16000
+valores = {
+            "queryInput":{
+                "audioConfig": {
+                    # FLAC: AUDIO_ENCODING_FLAC, WAV: AUDIO_ENCODING_LINEAR_16
+                    "audioEncoding": "AUDIO_ENCODING_FLAC",
+                    "sampleRateHertz": 16000,
+                    "languageCode": "es"
+                }
+            },
+            "inputAudio": audioBase64
+        }
+
+
+# Petición
+res = getIntent(valores, token)
+resJson = json.dumps(res.json(), indent=4)
+print(resJson.encode('ascii').decode('unicode-escape')) # Imprime correctamente los caracteres
---
+++ ejemplos/ejemplo_eventos-texto.py
@@ -0,0 +1,51 @@
+import requests
+import json
+import subprocess
+
+
+def obtenemosToken():
+    # Obtenemos token de google (Ver documentación de como generar el token)
+    token = subprocess.check_output(["gcloud", "auth", "print-access-token"])
+    token = token.decode("utf-8").strip()
+    return token
+
+
+def sendEvent(jdata, token):
+    # ---------------------------------------------------------------------
+    headers = {"Content-Type": "application/json",
+               "Authorization": "Bearer " + token,
+               "charset": "UTF-8"}
+    req = requests.post("https://dialogflow.googleapis.com/v2/projects/transferenciaautomatica2/agent/sessions/4fe0f059:detectIntent",
+                        data=json.dumps(jdata, indent=4), headers=headers)
+    return req
+
+
+
+# Programa principal
+token = obtenemosToken()
+
+# Actualizar los valores dependiendo el evento a procesar
+# https://dialogflow.com/docs/reference/api-v2/rest/v2/projects.agent.sessions/detectIntent
+valores = {
+            "queryInput": {
+                # Aquí puede descomentarse cualquiera de los dos bloques de
+                # código. Sirve para enviar eventos o textos.
+                # "event": {
+                #     'name': 'e_prueba',
+                #     'parameters': { 'nombre': 'Gabriel' },
+                #     'languageCode': 'en'
+                # }
+                "text": {
+                    "text": "Quiero saber el saldo de Gabriela",
+                    "languageCode": "es"
+                }
+            },
+            "queryParams": {
+                "timeZone": "America/Mexico_City"
+            }
+        }
+
+# Salta al evento
+res = sendEvent(valores, token)
+resJson = json.dumps(res.json(), indent=4)
+print(resJson.encode('ascii').decode('unicode-escape')) # Imprime correctamente los caracteres
