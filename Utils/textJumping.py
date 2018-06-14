32b43ee60acba54bbaefcc6a627a997e12fc8cab
ramonapariciog <moncho_apa@hotmail.com>
2018-06-13 21:38:16 -0500

    saltando con texto

--- Utils/textJumping.py
+++ Utils/textJumping.py
@@ -1,17 +1,18 @@
 import dialogflow
+import os
 
 def detect_intent_texts(project_id, session_id, texts, language_code):
     """Returns the result of detect intent with texts as inputs.
-
-    Using the same `session_id` between requests allows continuation
-    of the conversaion."""
+    Using the same `session_id` between requests allows to continue
+    into the conversaion."""
+    ruta = "../DATA/TransferenciaAutomatica2-9c525654a2fb_admin.json"
+    os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = ruta
     session_client = dialogflow.SessionsClient() # Implícita
 
-    #print(dir(session_client))
+    # print(dir(session_client))
 
     session = session_client.session_path(project_id, session_id)
     print('Session path: {}\n'.format(session))
-
     for text in texts:
         text_input = dialogflow.types.TextInput(
             text=text, language_code=language_code)
@@ -29,5 +30,6 @@ def detect_intent_texts(project_id, session_id, texts, language_code):
         print('Fulfillment text: {}\n'.format(
             response.query_result.fulfillment_text))
 
-
-detect_intent_texts("transferenciaautomatica2", "misesion", ["quiero hablar con Javier", "Javier Escalante"], "es")
+if __name__ == "__main__":
+    detect_intent_texts("transferenciaautomatica2", "089ca6d2-c6dc-9772-861b-ab98ba0ab24f",
+                        ["quiero hablar con Javier", "Javier Escalante"], "es")
