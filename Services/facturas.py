d60da4321538b7e79af0baab60746498f41ce5de
Erick Victor Gabriel Osorio Merlos <evgom.sid@gmail.com>
2018-06-26 16:57:20 -0500

    Borrado código no necesario para el servicio de voz para facturas.

--- Services/facturas.py
+++ Services/facturas.py
@@ -3,31 +3,6 @@ from .b2bcliente import Regexseaker
 import json
 import datetime
 
-def dudasFacturasCampos(campo):
-    entrada = "Para factura, "
-    campos = {
-        "documento": entrada + "el campo Tipo Documento como su nombre lo indica "
-                    "hace referencia a las variantes de documento con que cuenta "
-                    "el sistema, en este momento se encuentran disponibles "
-                    "documentos de tipo Factura o Nota de Crédito.",
-        "estado": entrada + "el campo Estatus indica los posibles estados en los "
-                    "cuales se encuetra el documento, siendo estos: "
-                    "Recibido, Firmado, Aceptado, Rechazado, Error o Enviado.",
-        "prefijo": entrada + "el campo Serie (Prefijo) identifica los documentos "
-                    "por un nivel general de organización, en este caso se "
-                    "representa mediante un valor alfanumérico, por ejemplo puede "
-                    "ser la serie B.",
-        "acuse": entrada + "el campo Acuse indica el estado de recepción del "
-                    "documento pudiendo ser Aceptado, Rechazado o Pendiente.",
-        "numero": entrada + "el campo Número de Factura se refiere al identificador "
-                    "específico del documento, este valor acepta números y letras. ",
-        "periodo": entrada + "el campo Periodo hace referencia a valores de "
-                    "tiempo predefinidos, siendo estos: Hoy, Semana y Mes."
-        }
-    resp = campos.get(campo)
-    return {"fulfillmentText": resp}
-
-
 def factura(req):
     # Ya que los elemenetos vienen en una lista deben tener un tratamiento
     # particular
