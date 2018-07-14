ea022c9fb9f57b3cb4180299a6e376d8767e8fbf
Erick Victor Gabriel Osorio Merlos <evgom.sid@gmail.com>
2018-07-14 02:07:22 -0500

    Comentarios para desactivar de webhook_dialogflow.
    Con estos comentarios se pueden desactivar facilmente el IM del ChatBot
    Wrapper.

--- webhook_dialogflow.py
+++ webhook_dialogflow.py
@@ -6,6 +6,7 @@ import os
 import sys
 from flask import Flask, request, make_response
 import requests
+#from Services import makeWebhookResult
 
 def post_data(jdata, link="http://0.0.0.0:5050/infomanager"):
     r = requests.post(link, data=jdata)
@@ -22,6 +23,7 @@ def retornodummy():
 @app.route("/webhook", methods=["POST", "GET"])
 def webhook():
     req = request.get_json(silent=True, force=True)
+    #res = makeWebhookResult(req)
     res = post_data(json.dumps(req))
     res = json.dumps(res, indent=4)
     r = make_response(res)
