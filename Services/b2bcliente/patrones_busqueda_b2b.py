bd8ae335167dd1e391c6eec045578c4c784d4f33
Erick Victor Gabriel Osorio Merlos <evgom.sid@gmail.com>
2018-07-16 12:29:46 -0500

    Primeros ajustes para valores inválidos de folios.

--- Services/b2bcliente/patrones_busqueda_b2b.py
+++ Services/b2bcliente/patrones_busqueda_b2b.py
@@ -141,10 +141,11 @@ class Regexseaker:
                                  NP: {<.*> <vssp3s0|vsis3s0|Es|da0ms0|cs>{1,3} <Acuse>}
                              """,
                       Folio=r""" Q: {<Es|sps00|da0ms0|unknown|Valor|cs|cc|Reciente|p0300000|dp1msp|spcms|dp1css>}
-                                 NP: {<Folio> <Q>* (<tipoFolio> <Q|Prefijo>*){1,2} <dato>}
-                                 NP: {<Folio> <Q>* (<dato> <Q|Prefijo>*){1,2} <tipoFolio>}
-                                 NP: {<dato> <Q>+ <Folio> <Q>* <tipoFolio>}
-                                 NP: {<tipoFolio> <Q>* <Folio> <Q>* <dato>}
+                                 NP: {<Folio> <Q>{0,3} (<tipoFolio> <Q|Prefijo>*){1,2} <dato>}
+                                 NP: {<Folio> <Q>{0,3} (<dato> <Q|Prefijo>*){1,2} <tipoFolio>}
+                                 NP: {<dato> <Q>{1,3} <Folio> <Q>{0,3} <tipoFolio>}
+                                 NP: {<tipoFolio> <Q>{1,3} <Folio> <Q>{0,3} <dato>}
+                                 NP: {<Folio> <Q>{0,3} (<tipoFolio> <Q|Prefijo>*){1,2} <.*>}
                              """,
                       Periodo=r""" DP: {<semana|dia|mes|año>}
                                    NP1: {<presente|pasado> <DP>}
@@ -562,27 +563,161 @@ class Regexseaker:
         return result
 
 
-if __name__ == "__main__":
-    reg = Regexseaker()
-    expr = [
-        "Quiero facturas de estado aceptado con número de documento 33443 y el prefijo es x",
-        "Quiero facturas de estado recibido con número de documento 33443 y el prefijo es x",
-        "Quiero facturas de estado error con número de documento 33443 y el prefijo es x",
-        "Quiero facturas de estado firmado con número de documento 33443 y el prefijo es x",
-        "Quiero facturas de estado enviado con número de documento 33443 y el prefijo es x",
-        "Quiero facturas de estado xsdsfds con número de documento 33443 y el prefijo es x",
-        "Quiero facturas de estado 12321 con número de documento 33443 y el prefijo es x",
-        "Quiero facturas de estado es recibido con número de documento 33443 y el prefijo es x",
-        "Quiero facturas de estado es el que sea con número de documento 33443 y el prefijo es x",
-        "Facturas de hoy con recibido como estado y prefijo algo",
-        "Facturas de hoy con recibido es estado y prefijo algo",
-        "Facturas de hoy con recibido estado y prefijo algo",
-        "Facturas de hoy con recibido es el estado y prefijo algo"
-        ]
-    print("\n")
 
-    print("\n")
-    for e in expr:
-        resultado = reg.seakexpresion(e.lower(), "Estado", nl=5)
-        print("{0}\nEstado: {1}\n".format(e, resultado))
-    # print("NoDocumento: ", reg.seakexpresion(expr.lower(), "NoDocumento", nl=3))
+def pruebasFolios():
+    reg = Regexseaker()
+
+    expsMalas = [
+        "y con folio de inicio igual a xxxxx",
+        "con rango de folio  entre el inicial de AAAAA",
+        "REWEWETW  es el folio de inicio",
+        "El folio de inicio es el ERWERWREWT",
+        "En folio de inicio asignamos el QWEERE",
+        "Para folio de inicio el valor de QWERWERWER",
+        "cuyo folio de inicio es el WQERRTW",
+        "folio de inicio igual a QWERRT",
+        "El folio de inicio definido en QWERTTW",
+    ]
+
+    expsBuenas_FolioInicio = [
+        "facturas donde el folio de inicial es 0123",
+        "y con folio de inicio igual a 000012",
+        "con rango de folio  entre el inicial de 123456789",
+        "9037432  es el folio de inicio",
+        "El folio de inicio es el 002930200",
+        "En folio de inicio asignamos el 2373893",
+        "Para folio de inicio el valor de 11111111",
+        "cuyo folio de inicio es el 222222",
+        "folio de inicio igual a 78923430",
+        "El folio de inicio definido en 23432",
+        "con folio inicial igual a 568768",
+        "con folio inicial  de  709898798",
+        "el folio inicial es el  689778",
+        "folio inicial de 6987687",
+        "folio Inicial definido en 79876767878 ",
+        "23432 es el folio de inicio",
+        "y asignamos 56987 como folio inicial",
+        "cuyo folio mas reciente de inicio es el 5687",
+        "6987898 es el ultimo folio de inicio",
+        "el folio inicial es el 6987879",
+        "tenemos un folio de inicialización de 679876",
+        "y donde 6987867 es el folio de inicio definido",
+        "con folio inicial asignado de 569876",
+        "folio de inicialización de 69879879",
+        "folio de inicio asignado a 5986987",
+        "dame las facturas con folio de inicio igual a 6987987",
+        "con comienzo de folio en 69887",
+        "con inicialización de folio o folio de inicio de 69979",
+        "Ademas de que el folio de comienzo de serie es el 60978",
+        "el folio de comienzo es el 7098",
+        "y un folio inicial o de inicio de 709098",
+        "con folio con principio de serie de 698987",
+        "dame las facturas que principian con el folio 09900909",
+        "Quiero las facturas con inicialización de folio en 97980990",
+        "Requiero las facturas cerradas pero que inicien con el folio 6899879",
+        "por favor factoras con inicialización de folio en 89989",
+        "notas de credito cuyo folio inicial es el 9889897979",
+        "908908908 como folio de inicialización",
+        "908908908 como folio de inicio",
+        "908908908 es el folio de inicio",
+        "dame las facturas que inician con el folio 987879797897",
+        "quiero facturas con folio inicial o de inicio definido en  6999897979",
+        "necesito las facturas donde el folio de comienzo es 433554",
+        "necesito las facturas que comienzan con el folio 7998989",
+        "solicito las facturas cuo folio de inicia empieza en 7898908",
+        "quiero facturas que empiezan con el folio 68799889",
+        "el folio 79898798 es el de inicio"
+    ]
+
+    expsBuenas_FolioFin = [
+        "con folio de finalización igual a 69898",
+        "el folio de finalizado es 6980987",
+        "el folio de fin es el 698798",
+        "con folio de fin en 6987",
+        "el folio de finalizacion es 98798787",
+        "en folio de finalización asignamos 6988",
+        "con folio de finalizado igual a 232343",
+        " y el folio final de 65656",
+        "el folio de finalización es el 12342",
+        "con folio de finalización de 98573",
+        "en folio de finalización ponemos 2334",
+        "y 87664 como folio de finalización",
+        "el folio final es 42445",
+        "con folio de finalización definido en 42455",
+        "el folio de finalización es 62564",
+        "en folio de finalización asignamos el 9873",
+        "9877 es el folio de finalización",
+        "53457 es el folio de finalizado",
+        "y con folio de finalización o de fin en 69887",
+        "el folio final es igual a 2334544",
+        "el folio termina en 6798",
+        "con folio final definido a 698787",
+        "y un folio de finalización igual a 23443",
+        "asi como un folio de fin de 6797898989",
+        "56987987 es el folio de finalización",
+        "56987987 es el folio de finalización",
+        "56987987 es el folio de cierre",
+        "como folio de cierre o de fin ponemos el 698987",
+        "el folio de fin es el 798988",
+        "679897 es el ultimo folio de finalizado",
+        "el reciente folio de finalizado es el 9898787",
+        "el folio 79898798 es el de finalización",
+        "como finalización de folio ponemos el 698988",
+        "y con folio de fin definido en 69887",
+        "como folio de finalizado se tiene el 69898",
+        "en folio de finalizado asignamoe el valor 609898",
+        "para el folio de finalizado ponemos 435665",
+        "9880708 es el valor del folio de final",
+        "11111 es nuestro folio de finalización",
+        "11111 es mi folio de finalización",
+        "mi folio de finalización es el 222222",
+        "nuestro valor de finalización en folio es el 697988",
+        "folio de finalizado es 689080880",
+        "para folio de finalizado asignamos el 67879799",
+        "en el cierre de folio ponemos el 68808080808",
+    ]
+
+    # for phrase in expsBuenas_FolioInicio:
+    #     resultado = folios(phrase, "Inicio")
+    #     if resultado[1] == 0:
+    #         print("Resulado: {1}\n{0}\n\n".format(str(resultado[0:2]), resultado[2]))
+    #
+    # for phrase in expsBuenas_FolioFin:
+    #     resultado = folios(phrase, "Fin")
+    #     if resultado[1] == 0:
+    #         print("Resulado: {1}\n{0}\n\n".format(str(resultado[0:2]), resultado[2]))
+
+    # for phrase in expsBuenas_FolioFin:
+    #     print("Resulado: {0}\n".format(str(folios(phrase, "Fin"))))
+    for phrase in expsMalas: #+ expsBuenas_FolioInicio:
+        resultado = reg.seakexpresion(phrase, "FolioInicio")
+        if resultado[1] == 0:
+            print("Resulado: {0}\n{1}\n".format(str(resultado[:2]),resultado[3]))
+
+
+if __name__ == "__main__":
+    # reg = Regexseaker()
+    # expr = [
+    #     "Quiero facturas de estado aceptado con número de documento 33443 y el prefijo es x",
+    #     "Quiero facturas de estado recibido con número de documento 33443 y el prefijo es x",
+    #     "Quiero facturas de estado error con número de documento 33443 y el prefijo es x",
+    #     "Quiero facturas de estado firmado con número de documento 33443 y el prefijo es x",
+    #     "Quiero facturas de estado enviado con número de documento 33443 y el prefijo es x",
+    #     "Quiero facturas de estado xsdsfds con número de documento 33443 y el prefijo es x",
+    #     "Quiero facturas de estado 12321 con número de documento 33443 y el prefijo es x",
+    #     "Quiero facturas de estado es recibido con número de documento 33443 y el prefijo es x",
+    #     "Quiero facturas de estado es el que sea con número de documento 33443 y el prefijo es x",
+    #     "Facturas de hoy con recibido como estado y prefijo algo",
+    #     "Facturas de hoy con recibido es estado y prefijo algo",
+    #     "Facturas de hoy con recibido estado y prefijo algo",
+    #     "Facturas de hoy con recibido es el estado y prefijo algo"
+    #     ]
+    # print("\n")
+    #
+    # print("\n")
+    # for e in expr:
+    #     resultado = reg.seakexpresion(e.lower(), "Estado", nl=5)
+    #     print("{0}\nEstado: {1}\n".format(e, resultado))
+    # # print("NoDocumento: ", reg.seakexpresion(expr.lower(), "NoDocumento", nl=3))
+
+    pruebasFolios()
