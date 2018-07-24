17dabf18a8dfd074dc14ee7b82ded0d81c98eb7b
ramonapariciog <moncho_apa@hotmail.com>
2018-07-24 17:21:27 -0500

    Revisa los comentarios Gabriel

--- ChatbotWrapper/Utils/methods_DF_to_IM.py
+++ ChatbotWrapper/Utils/methods_DF_to_IM.py
@@ -1,4 +1,5 @@
 import requests
+import json
 
 IM_FIELDS = "IM_fields"
 IM_LINK = "http://localhost:5050/infomanager"
@@ -12,5 +13,10 @@ def post_data(jdata, link=IM_LINK):
         print("Falló la comunicación con IM.")
 
 def updateIM(req:dict, fields: dict):
+    # Gabriel revisa la forma en la que estas definiendo a req
+    # y a fields en los parametros de la funcion
     req.update({IM_FIELDS: fields})
     post_data(req)
+    # Recuerda que para mandar la peticion de busquedas a im
+    # tienes que pasar el diccionario a json
+    # con json.dumps(diccionario)
