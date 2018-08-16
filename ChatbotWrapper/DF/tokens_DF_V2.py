1ee62cf84a9d0612370e5395adb6cc4472624f1f
Erick Victor Gabriel Osorio Merlos <evgom.sid@gmail.com>
2018-08-16 10:34:14 -0500

    Implementado un caché de tokens.

--- ChatbotWrapper/DF/tokens_DF_V2.py
+++ ChatbotWrapper/DF/tokens_DF_V2.py
@@ -3,11 +3,26 @@ import json
 import os
 import sys
 import subprocess
+
 from flask_cors import CORS
 from flask import Flask, request, make_response
+from datetime import datetime
 
-CERT_FILE = "/home/gabriel/Documentos/Certificados/misitio_crt.pem"
-KEY_FILE = "/home/gabriel/Documentos/Certificados/misitio_key.pem"
+import time
+
+_CERT_FILE = "/home/gabriel/Documentos/Certificados/misitio_crt.pem"
+_KEY_FILE = "/home/gabriel/Documentos/Certificados/misitio_key.pem"
+
+# Paths for json files
+_DICT_AGENT_CLIENT = {
+    "facturasvoz-estable": {
+        "paginaWeb": "/home/gabriel/Documentos/Keys_DF/facturasvoz-estable-ae59624e7be6_cliente.json"
+    }
+}
+
+# Dict Tokens
+_dicTokens = {}
+
 
 app = Flask(__name__)
 CORS(app)
@@ -37,13 +52,17 @@ def webhook():
 
 
 def getToken(req: dict):
-    print("{} - {} request token for {} agent.".format(req["client"], req["session"], req["agent"]))
+    print("{} - {} requesting token for {} agent.".format(req["client"], req["session"], req["agent"]))
 
-    _setJson(req["agent"], req["client"])
+    token = _getCachedToken(req["agent"], req["client"])
 
-    # Obtenemos token de google (Ver documentación de como generar el token)
-    token = subprocess.check_output(["gcloud", "auth", "application-default", "print-access-token"])
-    token = token.decode("utf-8").strip()
+    if not token:
+        _setJson(req["agent"], req["client"])
+        # Obtenemos token de google (Ver documentación de como generar el token)
+        token = subprocess.check_output(["gcloud", "auth", "application-default", "print-access-token"])
+        token = token.decode("utf-8").strip()
+        _setTokenCached(req["agent"], req["client"], token)
+
 
     print("{} - {} token: {}".format(req["client"], req["session"], token))
 
@@ -51,17 +70,49 @@ def getToken(req: dict):
 
 
 def _setJson(agent: str, client: str):
-    # Paths for json files
-    DICT_AGENT_CLIENT = {
-        "facturasvoz-estable": {
-            "paginaWeb": "/home/gabriel/Documentos/Keys_DF/facturasvoz-estable-ae59624e7be6_cliente.json"
+    os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = _DICT_AGENT_CLIENT[agent][client]
+
+
+def _getCachedToken(agent: str, client: str):
+    now = datetime.now()
+
+    if agent in _dicTokens and client in _dicTokens[agent] \
+            and (now - _dicTokens[agent][client]["time"]).seconds < 3500:
+        return _dicTokens[agent][client]["token"]
+    else:
+        return None
+
+
+def _setTokenCached(agent: str, client: str, token: str):
+    now = datetime.now()
+
+    # Build new dictonary for tokens
+    if not _dicTokens.get(agent):
+        _dicTokens[agent] = {}
+
+    _dicTokens[agent] = {
+        client: {
+            "time": now,
+            "token": token
         }
     }
 
-    os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = DICT_AGENT_CLIENT[agent][client]
+
+def _test():
+    req = {"agent": "facturasvoz-estable",
+           "client": "paginaWeb",
+           "session": "session1"}
+
+    for i in range(2):
+        getToken(req)
+        time.sleep(2)
+        print(_dicTokens)
+
 
 
 if __name__ == "__main__":
+    # _test()
+
     if len(sys.argv) > 1:
         portnumber = int(sys.argv[1])
     else:
@@ -70,4 +121,4 @@ if __name__ == "__main__":
 
     print("Starting token server on port %d" %port)
     app.run(debug=True, port=port, host="0.0.0.0",
-            ssl_context=(CERT_FILE, KEY_FILE))
+            ssl_context=(_CERT_FILE, _KEY_FILE))
