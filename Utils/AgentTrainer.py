d8d617ca028e168d7421d97d9ca6943a6ecfd172
ramonapariciog <moncho_apa@hotmail.com>
2018-09-12 01:39:59 -0500

    Entrenador crea intent

--- Utils/AgentTrainer.py
+++ Utils/AgentTrainer.py
@@ -27,25 +27,33 @@ class AgentTrainer:
         else:
             msgerr = "Key for project {0} doesn't exists".format(project_id)
             raise FileNotFoundError(msgerr)
+        self.intents_client = dialogflow.IntentsClient()
+        self.parent = self.intents_client.project_agent_path(project_id)
 
-    def delete_intent(self, intent_id):
+    def delete_intent(self, display_name):
         project_id = self.project_id
-        intents_client = dialogflow.IntentsClient()
-        intent_path = intents_client.intent_path(project_id, intent_id)
-        intents_client.delete_intent(intent_path)
+        intents = [{field[0].camelcase_name: field[1]
+                    for field in intent.ListFields()}
+                   for intent in self.intents_client.list_intents(self.parent)]
+        intent_mask = [True if intent["displayName"] == display_name else False
+                       for intent in intents]
+        if any(intent_mask):
+            index = intent_mask.index(True)
+            name = intents[index]["name"]
+            self.intents_client.delete_intent(name)
+        else:
+            print("The intent {0} doesn't exists".format(display_name))
 
     def create_intent(self, display_name, training_phrases_parts,
                       message_texts):
         """Create an intent of the given intent type."""
-        project_id = self.project_id
-        intents_client = dialogflow.IntentsClient()
-        parent = intents_client.project_agent_path(project_id)
         training_phrases = []
         for training_phrases_part in training_phrases_parts:
             part = dialogflow.types.Intent.TrainingPhrase.Part(
                 text=training_phrases_part)
             # Here we create a new training phrase for each provided part.
-            training_phrase = dialogflow.types.Intent.TrainingPhrase(parts=[part])
+            training_phrase = dialogflow.types.Intent.TrainingPhrase(
+                parts=[part])
             training_phrases.append(training_phrase)
 
         text = dialogflow.types.Intent.Message.Text(text=message_texts)
@@ -56,13 +64,17 @@ class AgentTrainer:
             training_phrases=training_phrases,
             messages=[message])
         try:
-            response = intents_client.create_intent(parent, intent)
+            response = self.intents_client.create_intent(self.parent, intent)
         except:
-            print(intents_client.get_intent(display_name))
+            print("updating intent ...")
+            self.delete_intent(display_name)
+            response = self.intents_client.create_intent(self.parent, intent)
         # print('Intent created: {}'.format(response))
+        return response
 
 if __name__ == "__main__":
     atr = AgentTrainer("preguntasrespuestas-humansite")
-    tphrases = ["comete mis calzones", "chupame el pie"]
-    message_texts = ["Mejor tu comete los tuyos"]
+    tphrases = ["Jarvis", "Oye Jarvis", "Te hablo a tí",
+                "Jarvis me escuchas", "Jarvis dime algo"]
+    message_texts = ["Vete a molestar a otro lado, pinche escuincle!!!"]
     atr.create_intent("prueba", tphrases, message_texts)
