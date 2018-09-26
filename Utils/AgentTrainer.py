42db2be1bb9f9f5308a1cd3dcf9f622e8240bd25
ramonapariciog <moncho_apa@hotmail.com>
2018-09-26 15:03:41 -0500

    Entrenador automatico listo para probarse

--- Utils/AgentTrainer.py
+++ Utils/AgentTrainer.py
@@ -3,7 +3,7 @@ import dialogflow_v2 as dialogflow
 import json
 import sys
 
-class AgentTrainer:
+class Agent_Handler:
     def __init__(self, project_id, key_directory_folder="metadata",
                  key_type="admin"):
         self.project_id = project_id
@@ -29,23 +29,82 @@ class AgentTrainer:
             raise FileNotFoundError(msgerr)
         self.intents_client = dialogflow.IntentsClient()
         self.parent = self.intents_client.project_agent_path(project_id)
+        self.entityclient = dialogflow.EntityTypesClient()
+        self.entity_parent = self.entityclient.project_agent_path(project_id)
+
+    def search_element_by_name(self, displayName, list_elements):
+        elems = [{field[0].camelcase_name: field[1]
+                  for field in element.ListFields()}
+                 for element in list_elements]
+        mask = [True if element["displayName"] == displayName else False
+                for element in elems]
+        if any(mask):
+            index = mask.index(True)
+            name = elems[index]["name"]
+        else:
+            name = None
+        return name
+
+    def delete_entity_type(self, displayName):
+        """Delete entity type with the given entity type name."""
+        list_types = self.entityclient.list_entity_types(self.entity_parent)
+        path_entity_type = self.search_element_by_name(displayName,
+                                                       list_types)
+        if path_entity_type:
+            self.entityclient.delete_entity_type(path_entity_type)
+        else:
+            print("The entity type {0} doesn't exists".format(displayName))
+
+    def create_entity_type(self, displayName, kind):
+        """Create an entity type."""
+        entity_type = dialogflow.types.EntityType(display_name=displayName,
+                                                  kind=kind)
+        try:
+            response = self.entityclient.create_entity_type(self.entity_parent,
+                                                            entity_type)
+        except Exception as error:
+            if type(error).__name__ == "FailedPrecondition":
+                print("updating entity type {0} ...".format(displayName))
+                self.delete_entity_type(displayName)
+                response = self.entityclient.create_entity_type(
+                    self.entity_parent, entity_type)
+            else:
+                print(type(error).__name__, error)
+                response = None
+        print('Entity type created: \n{}'.format(response))
+
+    def create_entity(self, displayNameType, entity_value, synonyms):
+        """Create an entity of the given entity type."""
+        list_types = self.entityclient.list_entity_types(self.entity_parent)
+        path_entity_type = self.search_element_by_name(displayNameType,
+                                                       list_types)
+        synonyms = synonyms or [entity_value]
+        # Note: synonyms must be exactly [entity_value] if the
+        # entity_type's kind is KIND_LIST
+        if path_entity_type:
+            entity = dialogflow.types.EntityType.Entity()
+            entity.value = entity_value
+            entity.synonyms.extend(synonyms)
+            response = self.entityclient.batch_create_entities(
+                path_entity_type, [entity])
+            print('Entity created: {}'.format(response))
+        else:
+            print("The entity type {0} doesn't exists".format(displayNameType))
+            print("please create first the entity type with create_entity_type")
 
     def delete_intent(self, display_name):
         project_id = self.project_id
-        intents = [{field[0].camelcase_name: field[1]
-                    for field in intent.ListFields()}
-                   for intent in self.intents_client.list_intents(self.parent)]
-        intent_mask = [True if intent["displayName"] == display_name else False
-                       for intent in intents]
-        if any(intent_mask):
-            index = intent_mask.index(True)
-            name = intents[index]["name"]
-            self.intents_client.delete_intent(name)
+        list_intents = self.intents_client.list_intents(self.parent)
+        name_intent = self.search_element_by_name(display_name,
+                                                  list_intents)
+        if name_intent:
+            self.intents_client.delete_intent(name_intent)
         else:
             print("The intent {0} doesn't exists".format(display_name))
 
     def create_intent(self, display_name, training_phrases_parts,
-                      message_texts):
+                      message_texts, action=None, webhook_state=False,
+                      input_context_names=[], output_contexts=[]):
         """Create an intent of the given intent type."""
         training_phrases = []
         for training_phrases_part in training_phrases_parts:
@@ -58,11 +117,18 @@ class AgentTrainer:
 
         text = dialogflow.types.Intent.Message.Text(text=message_texts)
         message = dialogflow.types.Intent.Message(text=text)
-
+        # Mas informacion
+        # https://dialogflow-python-client-v2.readthedocs.io/en/latest/gapic/ \
+        # v2/types.html#dialogflow_v2.types.Intent
+        # el tema de los contextos tambien tiene que tratarse
         intent = dialogflow.types.Intent(
             display_name=display_name,
             training_phrases=training_phrases,
-            messages=[message])
+            messages=[message],
+            action=action,
+            webhook_state=webhook_state,
+            input_context_names=input_context_names,
+            output_contexts=output_contexts)
         try:
             response = self.intents_client.create_intent(self.parent, intent)
         except Exception as error:
@@ -78,10 +144,48 @@ class AgentTrainer:
         return response
 
 if __name__ == "__main__":
-    atr = AgentTrainer("hs-preguntasrespuestas-fac0e")
-    # atr = AgentTrainer("facturasvoz-estable")
-    # atr = AgentTrainer("preguntasrespuestas-humansite")
-    tphrases = ["Jarvis", "Oye Jarvis", "Te hablo a tí",
-                "Jarvis me escuchas", "Jarvis dime algo"]
-    message_texts = ["Vete a molestar a otro lado, pinche escuincle!!!"]
-    atr.create_intent("prueba", tphrases, message_texts)
+    project_id = "hs-preguntasrespuestas-fac0e"
+    # handler = Agent_Handler("facturasvoz-estable")
+    # handler = Agent_Handler("preguntasrespuestas-humansite")
+    handler = Agent_Handler(project_id)
+    # Frases de entrenamiento
+    tphrases = ["Quiero unos tacos de cochinita",
+                "se me antoja una torta",
+                "Voy a preparar mole de olla",
+                "voy a preparar aguachile",
+                "Me das unas enfrijoladas",
+                "Tengo antojo de una birria",
+                "Tengo antojo de comerme un pozole",
+                "Tengo mucha hambre y quiero barbacoa"]
+    # Mensaje de respuesta
+    message_texts = ["Tienes hambre muchacho y quieres comer $comida.original"]
+    # Nombre de intent
+    display_name_intent = "auto-comer"
+    # action
+    action = "hambre"
+    # comunicacion con webhook
+    webhook_state = False
+    # Primero crear intent con especificaciones, si el intent ya existe
+    # lo borra y crea uno nuevo con las nuevas especificaciones
+    handler.create_intent(display_name_intent, tphrases, message_texts,
+                          action=action, webhook_state=webhook_state)
+    # Nombre de tipo de entity
+    display_name = "comida"
+    # Tipo de entity (hay listas 2, maps 1 o enumerables 3)
+    kind= 1  # KIND_MAP
+    # Valor de la entity
+    entity_value1 = "garnacha"
+    # Sinonimos para la entity
+    synonyms1 = ["huarache", "torta", "enfrijolada", "sope", "taco", "tacos",
+                 "quesadilla"]
+    # lo mismo
+    entity_value2 = "platillo"
+    synonyms2 = ["mole", "aguachile", "birria", "pancita", "cochinita",
+                 "barbacoa"]
+    entity_value3 = "golosina"
+    synonyms3 = ["cocada", "palanqueta", "gomitas", "cacahuates", "pepitas",
+                 "pistaches"]
+    handler.create_entity_type(display_name, kind)
+    handler.create_entity(display_name, entity_value1, synonyms1)
+    handler.create_entity(display_name, entity_value2, synonyms2)
+    handler.create_entity(display_name, entity_value3, synonyms3)
